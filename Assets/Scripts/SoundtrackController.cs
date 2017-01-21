1704426fa58b9715ab5b1a6b824d1f1eefe67bd2
Ashley BlakeHood <ashleyblakehood@gmail.com>
2017-01-21 22:42:15 +0000

    Soundtrack and Lazors

---
+++ Assets/Scripts/SoundtrackController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundtrackController : MonoBehaviour {
+
+    public List<AudioClip> SoundTrack;
+
+    private AudioSource Player;
+
+	// Use this for initialization
+	void Start () {
+        Player = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(!Player.isPlaying)
+        {
+            var clip = Random.Range(0, SoundTrack.Count);
+            Player.clip = SoundTrack[clip];
+            Player.Play();
+        }
+
+	}
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public AudioClip LazorCharge;
+    public AudioClip LazorFire;
+
     public float Distance = 50;
     public float Damage = 25;
 
@@ -11,12 +14,15 @@ public class Weapon : MonoBehaviour
 
     public GameObject LazorBall;
 
+    public AudioSource AudioSource;
+
     bool busy = false;
 
     bool firing = false;
 
     void Start()
     {
+        AudioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -44,18 +50,21 @@ public class Weapon : MonoBehaviour
 
         while(firing)
         {
-
+            AudioSource.clip = LazorCharge;
+            AudioSource.Play();
             while (lazor.transform.localScale.x < 1.5f)
             {
                 lazor.transform.localScale = Vector3.Lerp(lazor.transform.localScale, new Vector3(2.5f, 2.5f, 2.5f), 1f * Time.deltaTime);
                 yield return null;
             }
-
+            AudioSource.Stop();
             RaycastHit hit;
             Vector3 forward = transform.forward;
             Debug.DrawRay(transform.position, forward * 50, Color.green);
 
             var LazorLine = lazor.GetComponent<LineRenderer>();
+            AudioSource.clip = LazorFire;
+            AudioSource.Play();
             if (Physics.Raycast(transform.position, transform.forward, out hit, 50))
             {
                 var distance = Vector3.Distance(hit.transform.position, transform.position);
@@ -74,6 +83,7 @@ public class Weapon : MonoBehaviour
             LazorLine.enabled = false;
 
             Destroy(lazor);
+            AudioSource.Stop();
             firing = false;
         }
     }
