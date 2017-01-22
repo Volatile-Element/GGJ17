20b76ebd215a66e04103ac818f9d5dc74ae9a80f
Matthew Moore <matthewemmoore@googlemail.com>
2017-01-22 01:29:41 +0000

    Added code to check time of game to change the skybox and lighting

--- Assets/Scripts/SkyBoxController.cs
+++ Assets/Scripts/SkyBoxController.cs
@@ -12,60 +12,105 @@ public class SkyBoxController : MonoBehaviour
     public Material Skybox3;
     public Material Skybox4;
     public Material Skybox5;
+    public bool KeyOverride;
+
+    public int ThreeAM;
+    public int SixAM;
+    public int NineAM;
+    public int SixPM;
+    public int NinePM;
 
     // Use this for initialization
     void Start ()
     {
 		lights = GetComponentsInChildren<Light>();
+        KeyOverride = false;
+
+        ThreeAM = 3;
+        SixAM = 6;
+        NineAM = 9;
+        SixPM = 18;
+        NinePM = 21;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKey(KeyCode.Alpha1))
+        GetLightKeys();
+        
+        if(!KeyOverride)
         {
-            ResetLight();
-            var light = lights.FirstOrDefault(x => x.name.EndsWith("1"));
+            GetLightsTime();
+        }
+    }
 
-            light.enabled = true;
-            RenderSettings.skybox = Skybox1;
-            DynamicGI.UpdateEnvironment();
+    private void GetLightsTime()
+    {
+        int hour = DateTime.Now.Hour;
+
+        if (hour < ThreeAM)
+        {
+            UpdateSkybox("5", Skybox5);
+        }
+        else if (hour >= ThreeAM && hour < SixAM)
+        {
+            UpdateSkybox("1", Skybox1);
+        }
+        else if (hour >= SixAM && hour < NineAM)
+        {
+            UpdateSkybox("2", Skybox2);
+        }
+        else if (hour >= NineAM && hour < SixPM)
+        {
+            UpdateSkybox("3", Skybox3);
+        }
+        else if (hour >= SixPM && hour < NinePM)
+        {
+            UpdateSkybox("4", Skybox4);
+        }
+        else if (hour >= NinePM)
+        {
+            UpdateSkybox("5", Skybox5);
+        }
+
+    }
+
+    private void UpdateSkybox(string endsNumber, Material skybox)
+    {
+        ResetLight();
+        var light = lights.FirstOrDefault(x => x.name.EndsWith(endsNumber));
+
+        light.enabled = true;
+        RenderSettings.skybox = skybox;
+        DynamicGI.UpdateEnvironment();
+    }
+
+    private void GetLightKeys()
+    {
+        if(Input.GetKey(KeyCode.Alpha1))
+        {
+            KeyOverride = true;
+            UpdateSkybox("1", Skybox1);
         }
         else if(Input.GetKey(KeyCode.Alpha2))
         {
-            ResetLight();
-            var light = lights.FirstOrDefault(x => x.name.EndsWith("2"));
-
-            light.enabled = true;
-            RenderSettings.skybox = Skybox2;
-            DynamicGI.UpdateEnvironment();
+            KeyOverride = true;
+            UpdateSkybox("2", Skybox2);
         }
         else if(Input.GetKey(KeyCode.Alpha3))
         {
-            ResetLight();
-            var light = lights.FirstOrDefault(x => x.name.EndsWith("3"));
-
-            light.enabled = true;
-            RenderSettings.skybox = Skybox3;
-            DynamicGI.UpdateEnvironment();
+            KeyOverride = true;
+            UpdateSkybox("3", Skybox3);
         }
         else if(Input.GetKey(KeyCode.Alpha4))
         {
-            ResetLight();
-            var light = lights.FirstOrDefault(x => x.name.EndsWith("4"));
-
-            light.enabled = true;
-            RenderSettings.skybox = Skybox4;
-            DynamicGI.UpdateEnvironment();
+            KeyOverride = true;
+            UpdateSkybox("4", Skybox4);
         }
         else if(Input.GetKey(KeyCode.Alpha5))
         {
-            ResetLight();
-            var light = lights.FirstOrDefault(x => x.name.EndsWith("5"));
-
-            light.enabled = true;
-            RenderSettings.skybox = Skybox5;
-            DynamicGI.UpdateEnvironment();
+            KeyOverride = true;
+            UpdateSkybox("5", Skybox5);
         }
     }
 
